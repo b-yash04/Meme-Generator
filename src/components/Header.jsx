0937a347e5efbb7dd942adeda4b3265cf9fbd5cf
yash bhatt <bhatty048@gmail.com>
2025-07-14 22:54:12 +0530

    meme-generator-project-setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+            src="./src/trollface.png" 
+                alt="trollface"
+            />
+            <h1>Meme Generator</h1>
+        </header>
+    )
+}
